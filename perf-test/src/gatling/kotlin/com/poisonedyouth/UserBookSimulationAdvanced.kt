5dcbe20c7fb2523bfe1adf31e3d3f31db20d0a14
Matthias Schenk <matthias.schenk@sevdesk.de>
2023-03-21 09:13:38 +0100

    [V-2.4] Add UserBookSimulationAdvanced

---
+++ perf-test/src/gatling/kotlin/com/poisonedyouth/UserBookSimulationAdvanced.kt
@@ -0,0 +1,99 @@
+package com.poisonedyouth
+
+import io.gatling.javaapi.core.CoreDsl
+import io.gatling.javaapi.core.CoreDsl.StringBody
+import io.gatling.javaapi.core.CoreDsl.bodyString
+import io.gatling.javaapi.core.CoreDsl.feed
+import io.gatling.javaapi.core.Simulation
+import io.gatling.javaapi.http.HttpDsl
+import kotlin.random.Random
+
+private const val AMOUNT_INITIAL_DATA = 100
+
+class UserBookSimulationAdvanced : Simulation() {
+
+    private fun createCreateUserRequest() = HttpDsl.http("add user").post("/api/v1/user")
+        .body(
+            StringBody(
+                """
+                 {
+                     "firstName": "#{firstname}",
+                     "lastName": "#{lastname}",
+                     "birthDate" : "1999-01-01"
+                 }
+                """.trimIndent()
+            )
+        ).asJson()
+        .check(
+            bodyString().transform { body -> body.replace("\"", "") }.saveAs("userId")
+        )
+
+    private fun createCreateBookRequest() = HttpDsl.http("add book").post("/api/v1/book")
+        .body(
+            StringBody(
+                """
+                {
+                    "title": "#{title}",
+                    "author": "#{author}"
+                 }
+                 """.trimIndent()
+            )
+        ).asJson()
+        .check(bodyString().transform { body ->
+            body.replace("\"", "")
+        }.saveAs("bookId"))
+
+
+    private fun createAddBookRequest() = HttpDsl.http("add user book")
+        .post("/api/v1/user/#{currentUserId}/book")
+        .body(
+            StringBody(
+                """
+                {
+                    "books": #{currentBookIds}
+                }
+                """.trimIndent()
+            )
+        ).asJson()
+        .check(
+            HttpDsl.status().`is`(HTTP_STATUS_CREATED),
+        )
+        .check(
+            bodyString().exists()
+        )
+
+
+    private val addUserBookScenario = CoreDsl.scenario("Add book to user")
+        .repeat(AMOUNT_INITIAL_DATA)
+        .on(
+            feed(nameFeeder)
+                .feed(bookFeeder)
+                .exec(createCreateUserRequest())
+                .exec { session ->
+                    val userIds: List<String> = session.getList("userIds")
+                    session.set("userIds", userIds + session.get("userId"))
+                }
+                .exec(createCreateBookRequest())
+                .exec { session ->
+                    val bookIds: List<String> = session.getList("bookIds")
+                    session.set("bookIds", bookIds + session.get("bookId"))
+                }
+        )
+        .exec { session ->
+            val userIds: List<String> = session.getList("userIds")
+            val userId = userIds.random()
+            session.set("currentUserId", userId)
+        }
+        .exec { session ->
+            val bookIds: List<String> = session.getList("bookIds")
+            val bookIdsTaken = bookIds.shuffled().take(Random.nextInt(1, AMOUNT_INITIAL_DATA))
+            session.set("currentBookIds", bookIdsTaken.map { "\"$it\"" })
+        }
+        .exec(createAddBookRequest())
+
+    init {
+        setUp(
+            addUserBookScenario.injectOpen(CoreDsl.atOnceUsers(10))
+        ).protocols(httpProtocol)
+    }
+}
